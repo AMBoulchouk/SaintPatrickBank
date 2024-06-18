8b7927b1711931c8ca0b8a48c019fca6bdf4bce0
Alex M Boulchouk <amboulchouk@outlook.com>
2024-06-18 11:15:54 -0300

    V2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,7 +133,7 @@
                             class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
 
                             <div class="pt-3 sm:pt-5">
-                                <h2 class="text-xl font-semibold text-black dark:text-white">Join Us!</h2>
+                                <h2 class="text-xl font-semibold text-black dark:text-white">Open Account!</h2>
                                     <p class="mt-4 text-sm/relaxed">
                                         Join us today at Saint Patrick Bank and experience banking reimagined, where
                                         your needs come first and your satisfaction is our top priority.
